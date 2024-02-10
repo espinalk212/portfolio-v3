0b4c13e16e622917465859553f83c772360274ea
kespinal <espinalk212@gmail.com>
2024-02-10 18:57:55 -0500

    add loading component, vue pdf

---
+++ src/components/elements/Loading.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+const props = defineProps({
+  isLoading: {
+    type: Boolean,
+    default: false,
+  },
+  size: {
+    type: String,
+    default: 'default',
+    validator: (value) => ['small', 'default', 'large'].includes(value)
+  },
+  loadingMessage: {
+    type: String,
+    default: 'Loading...'
+  },
+})
+</script>
+
+<template>
+  <div class="w-full h-full">
+    <a-spin :spinning="isLoading" :size="props.size" :tip="props.loadingMessage"/>
+  </div>
+</template>
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,8 +1,26 @@
+<script setup>
+import { VuePDF, usePDF } from '@tato30/vue-pdf'
+import Loading from '@/components/elements/Loading.vue'
+import { ref, onMounted } from 'vue'
+
+const { pdf } = usePDF('../src/assets/Kevin_Espinal_Resume_Updated.pdf')
+const loading = ref(false)
+
+function onFileLoaded () {
+  console.log('done loading ...')
+  loading.value = false
+}
+
+onMounted(() => {
+  loading.value = true
+})
+</script>
+
+
 <template>
-  <div >
-    <h1>This is an about page</h1>
+  <div  class="w-full h-full">
+    <VuePDF class="flex justify-center" :pdf="pdf" @loaded="onFileLoaded" watermark-text="kevinespinal212" >
+      <Loading class="flex justify-center" size="large" :is-loading="loading"/>
+    </VuePDF>
   </div>
 </template>
-
-<style>
-</style>
